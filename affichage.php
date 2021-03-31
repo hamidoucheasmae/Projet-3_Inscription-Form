067d04d8aae09cac75461dcd27d581766ed164e3
hamidoucheasmae <hamidouche.asmae@gmail.com>
2021-03-31 09:59:41 +0100

    done

--- affichage.php
+++ affichage.php
@@ -111,7 +111,7 @@ foreach ($_SESSION["nom"] as $value) {
 ?>
 </table>
 </div>
-<button class="btn" onclick="window.print()">Print</button>
+<button onclick="window.print()">Print</button>
 	<!-- Optional JavaScript -->
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
