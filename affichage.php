b4ea0b9b44f617e3212b3485c590b31966c49cac
hamidoucheasmae <hamidouche.asmae@gmail.com>
2021-04-02 09:26:40 +0100

    push

--- affichage.php
+++ affichage.php
@@ -83,22 +83,22 @@ $_SESSION['nom'] =  $numero;
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand m-2" href="#">
-    <img src="final logo.png" alt="" width="Auto" height="60px"  class="d-inline-block align-top">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    </div>
-      <div class="navbar-nav">
-        <a class="nav-link" href="index.php">Page d'inscription</a>
-      </div>
-    
-    <div class="navbar-nav">
+  <a class="navbar-brand" href="#">
+<img src="final logo.png" alt="" width="Auto" height="60px"  class="d-inline-block align-top">
+</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Page d'inscription </a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" href="affichage.php">Liste des participants</a>
-      </div>
+      </li>
+  
+    </ul>
   </div>
 </nav>
 
