b5c104166487d3fdc1d3e73ab3d463856745d3e8
hamidoucheasmae <hamidouche.asmae@gmail.com>
2021-04-05 12:08:38 +0100

    more modification

--- Réalisation -Code Source/affichage.php
+++ Réalisation -Code Source/affichage.php
@@ -83,7 +83,7 @@ $_SESSION['nom'] =  $numero;
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">
+  <a class="navbar-brand m-2" href="#">
 <img src="final logo.png" alt="" width="Auto" height="60px"  class="d-inline-block align-top">
 </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
